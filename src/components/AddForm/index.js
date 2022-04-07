409e66a7f1e70f8a415ea0ff6dcc1e37a1c330fb
Solomii <kushniryksolomiya@gmail.com>
2022-04-07 16:15:10 +0300

    [#0] Create useState in my form

--- src/components/AddForm/index.js
+++ src/components/AddForm/index.js
@@ -1,12 +1,15 @@
-import React, {useState} from 'react'
-import { Book } from '../Book'
-import "./style.css"
+import React, { useState } from 'react';
+
+import "./style.css";
 
 export const Form = () => {
+  const [book, setBook] = useState({
+    title:"dsaf"
+  })
   return (
     <>
       <form className="books_form">
-        <input type="text" className="books_input" placeholder="enter book"></input>
+        <input type="text" className="books_input" placeholder="enter book" value={book.title}></input>
         <button type="submit"className="books_button">
           Add BOOK
         </button>
